583ed2f2dbec1e9b2422f0d919bc570c04386938
shawshawmee <jilcharlz009@gmail.com>
2023-02-06 19:58:57 +0800

    database seeders not final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,12 +23,15 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        for($i = 0; $i <10; $i++){
-            DB::table('students')->insert([
-                'name' => fake()->name,
-                'email' => fake()->email,
-                'age' => fake()->randomDigitNotNull,
-            ]);
-        }
+        // for($i = 0; $i <10; $i++){
+        //     DB::table('jobposts')->insert([
+        //         'user_id' => $user_id,
+        //         'jobtitle' => fake()->jobTitle,
+        //         'joblocation' => fake()->address,
+        //         'jobtype' => fake()->jobTitle,
+        //         'jobdescription' => fake()->bs,
+        //         'salary' => fake()->numberBetween($min = 25000, $max = 80000),
+        //     ]);
+        // }
     }
 }
